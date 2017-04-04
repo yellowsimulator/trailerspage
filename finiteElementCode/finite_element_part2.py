012cc6238e3f7d60df67e398e1a0853099fc99e5
Yapi Donatien Achou <ruben123x@gmail.com>
2017-04-04 17:49:28 +0200

    updates

--- finiteElementCode/finite_element_part2.py
+++ finiteElementCode/finite_element_part2.py
@@ -1,7 +1,11 @@
 """
 sample code for finite element method solving
 -u''(x) = f(x); u(0) = 0; u(1) = 0, x in [0,1]
+This code in not very efficient computationaly.
+It serves to illustrate the finite element method
+in practice.
 """
+from matplotlib import rc
 import matplotlib.pylab as plt
 from sympy import diff,Symbol,sin, lambdify, pi
 import scipy.integrate as integrate
@@ -9,20 +13,13 @@ from scipy.integrate import quad
 import numpy as np
 from numpy.linalg import inv
 
-N = 5
+N = 3
 x = Symbol('x')
 def phi(x,k):
     """
     basis functions
     """
     return sin( (k+1)*pi*x )
-
-
-def diff_phi(phi,x,k):
-    """
-    derivative of basis function
-    """
-    return diff(phi(x,k),x)
 
 
 def f(x):
@@ -35,7 +32,7 @@ def f(x):
     
 def u_e(x):
     """
-    Exct solution of 
+    Exact solution of 
     -u''(x) = f(x); u(0)=0; u(1)=0
     x in [0,1]
     """
@@ -46,24 +43,26 @@ A = np.zeros((N,N))
 b = np.zeros(N)
 for i in range(N):
     for j in range(N):
-        diff_phi_i = lambdify(x,diff_phi(phi,x,i))
-        diff_phi_j = lambdify(x,diff_phi(phi,x,j))
+        diff_phi_i = lambdify(x,diff(x,phi(x,i)))
+        diff_phi_j = lambdify(x,diff(x,phi(x,j)))
         A[i,j]= quad(lambda x: diff_phi_i(x)*diff_phi_j(x), 0,1)[0]
         b[j] = quad(lambda x: phi(x,j)*f(x), 0,1)[0]
         
 #compute C and the numerical solution u(x)
-m = range(N)
 C = inv(A).dot(b)   
-u = sum([C[i]*phi(x,k) for i, k in zip(m,m) ])
+u = sum([C[i]*phi(x,i) for i in range(N) ])
 u = lambdify(x,u,'numpy')
 
 #plot solution 
 z = np.linspace(0,1,N)
 ze = np.linspace(0,1,1000)
 
-plt.plot(ze,u_e(ze),z,u(z))
-plt.legend(['exact', 'numerical'])
-plt.title('Exact solution vs numerical solution for N={}'.format(N))
+#plt.plot(ze,u_e(ze),z,u(z))
+plt.plot(ze,u_e(ze))
+#plt.legend(['exact', 'numerical'])
+#plt.legend(['exact solution'])
+#plt.title('Exact solution vs numerical solution for N={}'.format(N))
+plt.title(r"Exact solution of $-u^{\prime\prime}(x) = x,\quad u(0)=u(1)=0$")
 plt.xlabel('x')
 plt.ylabel('u(x)')
 plt.show()
